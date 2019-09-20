d4740aec19167738c51d1a36b21f529606af6f68
Wolf Gp <45176998+haruka98@users.noreply.github.com>
2019-09-20 14:19:59 +0200

    added open/close process & inc_res

--- main.cpp
+++ main.cpp
@@ -52,3 +52,114 @@ int main(int argc, char* argv[]) {
 	close_process_mem(hProcess);
 	return 0;
 }
+
+HANDLE open_process_mem(char* process_window_title) {
+	PROCESSENTRY32 pentry;
+	pentry.dwSize = sizeof(PROCESSENTRY32);
+	HANDLE csnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, (DWORD)NULL);
+	if(Process32First(csnapshot, &pentry) == TRUE) {
+		while(Process32Next(csnapshot, &pentry) == TRUE) {
+			if(_stricmp(pentry.szExeFile, process_window_title) == 0) {
+				HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pentry.th32ProcessID);
+				pid = pentry.th32ProcessID;
+				CloseHandle(csnapshot);
+				if(hProcess == INVALID_HANDLE_VALUE) {
+					return hProcess;
+				}
+				return hProcess;
+			}
+		}
+	}
+	CloseHandle(csnapshot);
+	return INVALID_HANDLE_VALUE;
+}
+
+HANDLE open_process_mem_pid(DWORD set_pid) {
+	PROCESSENTRY32 pentry;
+	pentry.dwSize = sizeof(PROCESSENTRY32);
+	HANDLE csnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, (DWORD)NULL);
+	if(Process32First(csnapshot, &pentry) == TRUE) {
+		while(Process32Next(csnapshot, &pentry) == TRUE) {
+			if(pentry.th32ProcessID == set_pid) {
+				HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pentry.th32ProcessID);
+				pid = pentry.th32ProcessID;
+				CloseHandle(csnapshot);
+				if(hProcess == INVALID_HANDLE_VALUE) {
+					return hProcess;
+				}
+				return hProcess;
+			}
+		}
+	}
+	CloseHandle(csnapshot);
+	return INVALID_HANDLE_VALUE;
+}
+
+void close_process_mem(HANDLE hProcess) {
+	CloseHandle(hProcess);
+}
+
+
+uintptr_t GetModuleBaseAddress(DWORD procId, const char* modName) {
+	uintptr_t modBaseAddr = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, procId);
+	if(hSnap != INVALID_HANDLE_VALUE) {
+		MODULEENTRY32 modEntry;
+		modEntry.dwSize = sizeof(modEntry);
+		if(Module32First(hSnap, &modEntry)) {
+			do{
+				if(!strcmp(modEntry.szModule, modName)) {
+					modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+					break;
+				}
+			}while(Module32Next(hSnap, &modEntry));
+		}
+	}
+	CloseHandle(hSnap);
+	return modBaseAddr;
+}
+
+void inc_res(HANDLE hProcess, int res_type) {
+	uintptr_t moduleBase = GetModuleBaseAddress(pid, "Addon.exe");
+	uintptr_t addr = 0;
+	BYTE value[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// gold
+					0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};	// honor
+	ReadProcessMemory(hProcess, (LPVOID)(moduleBase + GOLDPTR_A), &addr, sizeof(addr), NULL);
+	ReadProcessMemory(hProcess, (LPVOID)(addr + GOLDPTR_B), &addr, sizeof(addr), NULL);
+	ReadProcessMemory(hProcess, (LPVOID)(addr + GOLDPTR_C), &addr, sizeof(addr), NULL);
+	ReadProcessMemory(hProcess, (LPVOID)(addr + GOLDPTR_D), &addr, sizeof(addr), NULL);
+	ReadProcessMemory(hProcess, (LPVOID)(addr + GOLDPTR_E), &addr, sizeof(addr), NULL);
+	ReadProcessMemory(hProcess, (LPVOID)(addr + GOLDPTR_F), value, 16, NULL);
+	if(res_type == 1) {	// gold
+		if(value[2] != 0xFF) {
+			value[2] = value[2] + 0x01;
+		} else {
+			value[2] = 0x00;
+			if(value[3] != 0xFF) {
+				value[3] = value[3] + 0x01;
+			} else {
+				value[3] = 0x00;
+				if(value[4] != 0xEE) {
+					value[4] = value[4] + 0x01;
+				}
+			}
+		}
+	}
+	if(res_type == 2) {	// honor
+		if(value[10] != 0xFF) {
+			value[10] = value[10] + 0x01;
+		} else {
+			value[10] = 0x00;
+			if(value[11] != 0xFF) {
+				value[11] = value[11] + 0x01;
+			} else {
+				value[11] = 0x00;
+				if(value[12] != 0xEE) {
+					value[12] = value[12] + 0x01;
+				}
+			}
+		}
+	}
+	WriteProcessMemory(hProcess, (LPVOID)(addr + GOLDPTR_F), value, 16, NULL);
+	return;
+}
