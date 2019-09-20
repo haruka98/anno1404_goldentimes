4471d32aa101e3da50f2fc00c7e2604d1218c2f4
Wolf Gp <45176998+haruka98@users.noreply.github.com>
2019-09-20 14:17:52 +0200

    added pointer & function declaration

--- main.hpp
+++ main.hpp
@@ -1,4 +1,19 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+#define GOLDPTR_A 0x00ED92E8
+#define GOLDPTR_B 0x34
+#define GOLDPTR_C 0xE8
+#define GOLDPTR_D 0x50
+#define GOLDPTR_E 0x0
+#define GOLDPTR_F 0xA0
+
+DWORD pid = 0;
+
+HANDLE open_process_mem(char* process_window_title);
+HANDLE open_process_mem_pid(DWORD set_pid);
+void close_process_mem(HANDLE hProcess);
+uintptr_t GetModuleBaseAddress(DWORD procId, const char* modName);
+void inc_res(HANDLE hProcess, int res_type);
+
 #endif
